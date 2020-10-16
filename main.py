b003c56f4c1f69b4f86c66f840b86d3075e675ac
lorenabc <71780826+lorenabc@users.noreply.github.com>
2020-10-15 22:51:19 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+print("********")
+print("Bem vindo ao Bacará!")
+print("********")
+
+fichas = 100
+
+print(f'Você começará o jogo com {fichas} fichas. Boa sorte')
+
+quantos_baralhos = int(input("Você quer jogar com quantos baralhos? (1/6/8)"))
+
+quantos_jogadores = int(input("Quantos jogadores vão participar? "))
