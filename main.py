af938b4931107a1d5a101ec7c417b5776cb7e865
Paulofalcao2002 <72179636+Paulofalcao2002@users.noreply.github.com>
2020-10-15 22:54:23 -0300

    Update main.py

--- main.py
+++ main.py
@@ -27,3 +27,19 @@ while game_on:
 
     print(texto_jogador)
     print(texto_banco)
+
+    while game_on:
+    quem_aposta = input('Em quem você deseja apostar? (jogador/banco/empate)')
+    fichas_apostadas = int(input("Quantas fichas você deseja apostar? "))
+
+
+    carta1_jogador, valor1_jogador = sorteia_carta(quantos_baralhos, baralho)
+    carta2_jogador, valor2_jogador = sorteia_carta(quantos_baralhos, baralho)
+    carta1_banco, valor1_banco = sorteia_carta(quantos_baralhos, baralho)
+    carta2_banco, valor2_banco = sorteia_carta(quantos_baralhos, baralho)
+
+    texto_jogador, soma_jogador = confere_terceira_carta(valor1_jogador, valor2_jogador, carta1_jogador, carta2_jogador, 'jogador')
+    texto_banco, soma_banco = confere_terceira_carta(valor1_banco, valor2_banco, carta1_banco, carta2_banco, 'banco')
+
+    print(texto_jogador)
+    print(texto_banco)
